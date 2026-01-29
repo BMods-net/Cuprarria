ac883e9e460b4d75939837f1a0dd5769f7bac24e
zhenya194 <zhenya.pro1753@gmail.com>
2026-01-29 15:03:28 +0200

    Add full set

---
+++ Content/Items/Armor/CupheadLegs.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Cuprarria.Content.Items.Armor
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class MagicLegs : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 18;
+            Item.value = Item.sellPrice(silver: 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.vanity = true;
+        }
+    }
+}
