e2e057465e3d3836cb30406bcbee1eeedd52d754
zhenya194 <zhenya.pro1753@gmail.com>
2026-01-30 12:23:02 +0200

    Add first weapon and projectile

---
+++ Content/Items/FirstMagicalAbility.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Cuprarria.Content.Items
+{
+    public class FirstMagicalAbility : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 40;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.autoReuse = true;
+
+            Item.noMelee = true;
+            Item.DamageType = DamageClass.Magic;
+            Item.damage = 30;
+            Item.knockBack = 3;
+
+            Item.mana = 0;
+
+            Item.shoot = ModContent.ProjectileType<Projectiles.ProjectileForFMA>();
+            Item.shootSpeed = 14f;
+
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(silver: 0);
+
+            Item.UseSound = SoundID.Item20;
+        }
+    }
+}
---
+++ Content/Projectiles/ProjectileForFMA.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Cuprarria.Content.Projectiles
+{
+    public class ProjectileForFMA : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+
+            Projectile.friendly = true;  
+            Projectile.hostile = false;
+
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.knockBack = 5f;
+
+            Projectile.timeLeft = 450;
+            Projectile.penetrate = 5;
+
+            Projectile.aiStyle = 1;      
+            AIType = ProjectileID.Bullet;
+        }
+    }
+}
