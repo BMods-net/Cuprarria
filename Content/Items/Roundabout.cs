a9773f6c3015f080ec464d1f07c4a0f64aa94dcb
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-01 10:44:00 +0200

    Add Roundabout and projectile for it

---
+++ Content/Items/Roundabout.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Cuprarria.Content.Items
+{
+    public class Chaser : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 40;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.autoReuse = true;
+
+            Item.noMelee = true;
+            Item.DamageType = DamageClass.Magic;
+            Item.damage = 20;
+            Item.knockBack = 0;
+
+            Item.mana = 0;
+
+            Item.shoot = ModContent.ProjectileType<Projectiles.ProjectileChaser>();
+            Item.shootSpeed = 14f;
+
+            Item.rare = ItemRarityID.Green;
+            Item.value = Item.buyPrice(silver: 0);
+
+            Item.UseSound = SoundID.Item20;
+        }
+    }
+}
